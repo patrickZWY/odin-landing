2db1a727b5763c5a13e532444d0f57c835bf5f49
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2024-01-09 18:23:01 -0500

    do querySelectorAll instead of individually

--- navlinks.js
+++ navlinks.js
@@ -1,3 +1,4 @@
+/*
 const btn = document.querySelector(`#firstlink`);
 btn.addEventListener('click', () => {
     alert("No Link Inserted Yet!");
@@ -6,11 +7,15 @@ const btn2 = document.querySelector(`#secondlink`);
 btn2.addEventListener('click', () => {
     alert("No Link Inserted Yet!");
 });
-/*
-const btn3 = document.querySelector(`#thirdlink`);
-btn3.onclick = () => alert("No Link Inserted Yet!");
-*/
+
 const btn3 = document.querySelector(`#thirdlink`);
 btn3.addEventListener('click', () => {
     alert("No Link Inserted Yet!");
+});
+*/
+const buttons = document.querySelectorAll('#navibar');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert("Link Not Inserted Yet!");
+    });
 });
