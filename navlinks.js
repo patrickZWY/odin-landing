758d5d8da1c5ccd5574a7290cba9ee73713b1043
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2024-01-09 17:13:54 -0500

    change onclick to use addEventListener

--- navlinks.js
+++ navlinks.js
@@ -1,6 +1,16 @@
 const btn = document.querySelector(`#firstlink`);
-btn.onclick = () => alert("No Link Inserted Yet!");
+btn.addEventListener('click', () => {
+    alert("No Link Inserted Yet!");
+});
 const btn2 = document.querySelector(`#secondlink`);
-btn2.onclick = () => alert("No Link Inserted Yet!");
+btn2.addEventListener('click', () => {
+    alert("No Link Inserted Yet!");
+});
+/*
 const btn3 = document.querySelector(`#thirdlink`);
 btn3.onclick = () => alert("No Link Inserted Yet!");
+*/
+const btn3 = document.querySelector(`#thirdlink`);
+btn3.addEventListener('click', () => {
+    alert("No Link Inserted Yet!");
+});
