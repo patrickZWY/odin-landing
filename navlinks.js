3b1b1963f53b078527f981874a9ee6e12e14754a
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2024-01-09 17:09:49 -0500

    insert link info for navilinks

---
+++ navlinks.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(`#firstlink`);
+btn.onclick = () => alert("No Link Inserted Yet!");
+const btn2 = document.querySelector(`#secondlink`);
+btn2.onclick = () => alert("No Link Inserted Yet!");
+const btn3 = document.querySelector(`#thirdlink`);
+btn3.onclick = () => alert("No Link Inserted Yet!");
